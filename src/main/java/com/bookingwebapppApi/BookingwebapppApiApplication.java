88fa426d90b93a1db37736635677da30288ac717
Azeez Ridwan <readone.cybernet@gmail.com>
2023-03-30 15:18:43 +0100

    first commit

---
+++ src/main/java/com/bookingwebapppApi/BookingwebapppApiApplication.java
@@ -0,0 +1,13 @@
+package com.bookingwebapppApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingwebapppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingwebapppApiApplication.class, args);
+	}
+
+}
