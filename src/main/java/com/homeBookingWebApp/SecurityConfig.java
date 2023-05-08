37fa530ec50121a899b55e0a58a0eceb5895aef2
Azeez Ridwan <readone.cybernet@gmail.com>
2023-05-08 16:54:37 +0100

    update 4.3

--- src/main/java/com/homeBookingWebApp/SecurityConfig.java
+++ src/main/java/com/homeBookingWebApp/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.homeBookingWebApp;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 import javax.sql.DataSource;
 
@@ -22,8 +22,6 @@ import org.springframework.security.web.authentication.rememberme.JdbcTokenRepos
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import com.homeBookingWebApp.CustomLoginHandler.CustomLoginFailureHandler;
-import com.homeBookingWebApp.CustomLoginHandler.CustomLoginSuccessHandler;
 import com.homeBookingWebApp.Service.UserSecurityService;
 import com.homeBookingWebApp.Utility.SecurityConstant;
 import com.homeBookingWebApp.Utility.SecurityUtility;
@@ -35,85 +33,65 @@ import com.homeBookingWebApp.Utility.Filter.JwtAuthorizationFilter;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-    @Autowired
-    private Environment env;
+	@Autowired
+	private Environment env;
 
-    @Autowired
-    private DataSource dataSource;
+	@Autowired
+	private DataSource dataSource;
 
-    @Autowired
-    private UserSecurityService userSecurityService;
+	@Autowired
+	private UserSecurityService userSecurityService;
 
-    @Autowired
-    private JwtAuthorizationFilter jwtAuthorizationFilter;
+	@Autowired
+	private JwtAuthorizationFilter jwtAuthorizationFilter;
 
-    @Autowired
-    private JwtAccessDeniedHandler jwtAccessDeniedHandler;
+	@Autowired
+	private JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    @Autowired
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-    private BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return SecurityUtility.passwordEncoder();
-    }
+	private BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return SecurityUtility.passwordEncoder();
+	}
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http
-                .getSharedObject(AuthenticationManagerBuilder.class);
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		AuthenticationManagerBuilder authenticationManagerBuilder = http
+				.getSharedObject(AuthenticationManagerBuilder.class);
 
-        authenticationManagerBuilder.userDetailsService(userSecurityService).passwordEncoder(bCryptPasswordEncoder());
-        
-        http
-		.authorizeHttpRequests(auth -> {
-			auth.antMatchers(SecurityConstant.PUBLIC_URLS).permitAll();
+		authenticationManagerBuilder.userDetailsService(userSecurityService).passwordEncoder(bCryptPasswordEncoder());
+
+		http.authorizeHttpRequests(auth -> {
+			auth.requestMatchers(SecurityConstant.PUBLIC_URLS).permitAll();
 			auth.anyRequest().authenticated();
 		});
-        http
-                .csrf(csrf -> csrf.disable())
-                .cors().and()
-                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .exceptionHandling(handling -> handling.accessDeniedHandler(jwtAccessDeniedHandler)
-                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)).addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
-                .logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/?logout")
-                        .permitAll()).rememberMe(me -> me
-                .tokenValiditySeconds(3 * 24 * 60 * 60).tokenRepository(persistentTokenRepository())); 
-            		
+		http.csrf(csrf -> csrf.disable()).cors(withDefaults())
+				.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.exceptionHandling(handling -> handling.accessDeniedHandler(jwtAccessDeniedHandler)
+						.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+				.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+				.logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+						.logoutSuccessUrl("/?logout").permitAll())
+		/*
+		 * .rememberMe(me -> me .tokenValiditySeconds(3 * 24 * 60 * 60)
+		 * .tokenRepository(persistentTokenRepository()))
+		 */ ;
 
+		return http.build();
+	}
 
-      /* http.authorizeRequests().antMatchers(SecurityConstant.PUBLIC_URLS).permitAll().anyRequest().authenticated(); */
+	@Bean
+	PersistentTokenRepository persistentTokenRepository() {
+		JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+		tokenRepository.setDataSource(dataSource);
+		return tokenRepository;
+	}
 
-       /* http.csrf().disable().cors().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().exceptionHandling().accessDeniedHandler(jwtAccessDeniedHandler)
-                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                .and().addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/?logout")
-                .permitAll().and().rememberMe()
-                .tokenValiditySeconds(3 * 24 * 60 * 60).tokenRepository(persistentTokenRepository()); */
-                        		
-
-
-        return http.build();
-    }
-
-    @Bean
-    PersistentTokenRepository persistentTokenRepository() {
-        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
-        tokenRepository.setDataSource(dataSource);
-        return tokenRepository;
-    }
-
-
-    @Bean
-    AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    @Autowired
-    private CustomLoginFailureHandler loginFailureHandler;
-
-    @Autowired
-    private CustomLoginSuccessHandler loginSuccessHandler;
+	@Bean
+	AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
 
 }
